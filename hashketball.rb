bb27344ca2a8d8d9b1cf3b49d7fd5b4a712e186c
Elvis Landi <elandi13@yahoo.com>
2020-11-11 23:51:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,18 +158,6 @@ def team_colors(team_name)
 end
   
 
-
-
-
-
-# def team_colors(team_name)
-#   game_hash.each do |home_away, keys|
-#     if keys[:team_name] == team_name
-#       return keys[:colors].map(&:capitalize)
-#     end
-#   end
-# end
-
 def team_names
   game_hash.map {|home_away, keys| keys[:team_name]}
 end
