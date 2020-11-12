8a70f0301ed6f42d0de2d1f97110357f3ce50dc9
Elvis Landi <elandi13@yahoo.com>
2020-11-12 00:05:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,6 @@ def player_stats(player_name)
 	game_hash.each do |location, team_data|
 		team_data[:players].each do |a| 
 			if a[:player_name] == player_name
-				my_hash = a
 				return my_hash
 			end
 		end
